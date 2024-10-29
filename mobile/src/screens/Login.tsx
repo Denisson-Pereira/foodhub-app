0558eb57d6c30e0e1434db008f63741c291730dd
Denisson Pereira Santos <denisson.pereira753@gmail.com>
2024-10-29 20:16:20 -0300

    Add login method

--- mobile/src/screens/Login.tsx
+++ mobile/src/screens/Login.tsx
@@ -1,9 +1,53 @@
-import { Text, View } from "react-native"
+import { useRef, useState } from "react"
+import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native"
+import { loginService } from "../services/loginService";
+import { useFoodHubContext } from "../context";
+import { useNavigate } from "../hooks/useNavigate";
 
 export const Login = () => {
+
+    const login = useRef<string>('');
+    const password = useRef<string>('');
+
+    const { setUser } = useFoodHubContext();
+
+    const { navigate } = useNavigate();
+
+    async function handleLogin() {
+        const date = await loginService(login.current, password.current, setUser);
+        if(date) {
+            setUser(date);
+            navigate('teste')
+            return date;
+        }
+
+    }
+
     return (
-        <View>
-            <Text>Login</Text>
+        <View style={styles.view}>
+            <TextInput 
+                keyboardType="email-address"
+                placeholder="login"
+                onChangeText={(text) => login.current = text}
+            />
+            <TextInput 
+                keyboardType="numeric"
+                placeholder="password"
+                onChangeText={(text) => password.current = text}
+            />
+            <TouchableOpacity
+                onPress={handleLogin}
+            >
+                <Text>LOGIN</Text>
+            </TouchableOpacity>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    view: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
